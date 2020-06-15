5b6b7d03f7a2cc658c1ccfddefa55a4f2a78f8ae
zalaps <zalaps@hotmail.com>
2020-06-15 06:52:23 +0530

    about us changes

--- try-react-rt/src/Components/About.js
+++ try-react-rt/src/Components/About.js
@@ -28,7 +28,7 @@ class About extends Component {
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2>BlackHole Values</h2>
                   <p className="address">
 						   <span>{name}</span><br />
 						   <span>{street}<br />
