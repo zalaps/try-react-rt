d611a5cb936a7720912857b82ec717ce9c7e5ac8
zalaps <zalaps@hotmail.com>
2020-06-16 07:03:21 +0530

    added values text in about

--- try-react-rt/src/Components/About.js
+++ try-react-rt/src/Components/About.js
@@ -14,35 +14,45 @@ class About extends Component {
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
+      var people = this.props.data.companyvalues.people;
+      var journey = this. props.data.companyvalues.journey;
+      var progress = this.props.data.companyvalues.progress;
     }
 
     return (
       <section id="about">
       <div className="row">
-         <div className="three columns">
+         {/* <div className="three columns">
             <img className="profile-pic"  src={profilepic} alt="Blackhole logo" />
-         </div>
-         <div className="nine columns main-col">
+         </div> */}
+         <div className="twelve columns main-col">
             <h2>About BlackHole</h2>
-
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>BlackHole Values</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
+                  <div class="row">
+                     <div class="column">
+                        <h3>People</h3>
+                        <p>{people}</p>
+                     </div>
+                     
+                     <div class="column">
+                        <h3>Journey</h3>
+                        <p>{journey}</p>
+                     </div>
+                     
+                     <div class="column">
+                        <h3>Progres</h3>
+                        <p>{progress}</p>
+                     </div>
+                  </div>
                </div>
-               <div className="columns download">
+               {/* <div className="columns download">
                   <p>
                      <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
-               </div>
+               </div> */}
             </div>
          </div>
       </div>
