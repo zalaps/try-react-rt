410a8fe5cce82be0b5c850df39a54b96ca9e3918
zalaps <zalaps@hotmail.com>
2020-07-03 23:34:02 +0530

    skills

--- try-react-rt/src/Components/Resume.js
+++ try-react-rt/src/Components/Resume.js
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 class Resume extends Component {
   render() {
     if (this.props.data) {
-      var skillmessage = this.props.data.skillmessage;
-
       var education = this.props.data.education.map(function (education) {
         return (
           <div key={education.school}>
@@ -18,22 +16,17 @@ class Resume extends Component {
         return (
           <div key={work.company}>
             <h3>{work.company}</h3>
-            <p className="info">
-              {work.title}
-              <span>&bull;</span> <em className="date">{work.years}</em>
-            </p>
-            <p>{work.description}</p>
+            <p className="justified">{work.description}</p>
           </div>
         );
       });
 
       var skills = this.props.data.skills.map(function (skills) {
-        var className = "bar-expand " + skills.name.toLowerCase();
         return (
-          <li key={skills.name}>
-            <span style={{ width: skills.level }} className={className}></span>
-            <em>{skills.name}</em>
-          </li>
+          <div key={skills.name}>
+            <h3>{skills.name}</h3>
+            <p>{skills.stacknames}</p>
+          </div>
         );
       });
     }
@@ -64,20 +57,14 @@ class Resume extends Component {
           <div className="nine columns main-col">{work}</div>
         </div>
 
-        <div className="row skill">
+        <div className="row work">
           <div className="two columns header-col">
             <h1>
               <span>Techstacks</span>
             </h1>
           </div>
 
-          <div className="nine columns main-col">
-            <p>{skillmessage}</p>
-
-            <div className="bars">
-              <ul className="skills">{skills}</ul>
-            </div>
-          </div>
+          <div className="nine columns main-col">{skills}</div>
         </div>
       </section>
     );
