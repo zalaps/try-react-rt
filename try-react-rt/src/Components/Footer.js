a41692579b3f5b73a38aef536f764fc947ed68af
pradyuman.zala <pradyuman.zala@kevision.in>
2020-07-08 16:49:17 +0530

    footer modification

--- try-react-rt/src/Components/Footer.js
+++ try-react-rt/src/Components/Footer.js
@@ -14,17 +14,24 @@ class Footer extends Component {
 
      <div className="row">
         <div className="twelve columns">
+          <ul>
+            <h2 className="footer">Contact Us</h2>
+          </ul>
            <ul className="social-links">
               {networks}
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 BlackHole</li>
+              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
-
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+        
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
      </div>
   </footer>
     );
