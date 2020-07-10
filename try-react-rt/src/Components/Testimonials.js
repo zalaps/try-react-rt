f280a54c723b8c6cfab46b2b85f07bf62ad086d1
pradyuman.zala <pradyuman.zala@kevision.in>
2020-07-10 12:27:15 +0530

    made testimonials responsive

--- try-react-rt/src/Components/Testimonials.js
+++ try-react-rt/src/Components/Testimonials.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Testimonials extends Component {
-   render() {
+  render() {
+    if (this.props.data) {
+      var testimonials = this.props.data.testimonials.map(function (testimonials) {
+        return (
+         <div key={testimonials.user}>
+            <p className="justified">{testimonials.text}</p>
+            <cite>{testimonials.user}</cite>
+         </div>
+        );
+      });
+    }
 
-      if (this.props.data) {
-         var testimonials = this.props.data.testimonials.map(function (testimonials) {
-            return <li key={testimonials.user}>
-               <blockquote>
-                  <p className="justified">{testimonials.text}</p>
-                  <cite>{testimonials.user}</cite>
-               </blockquote>
-            </li>
-         })
-      }
+    return (
+      <section id="testimonials">
+        <div className="row education">
+          <div className="two columns header-col">
+            <h1>
+              <span>Testimonials</span>
+            </h1>
+          </div>
 
-      return (
-         <section id="testimonials">
-            <div className="row work">
-               <div className="row">
-                  <div className="two columns header-col">
-                     <h1>
-                        <span>Client Testimonials</span>
-                     </h1>
-                  </div>
-
-                  <div className="ten columns main-col">
-                     <ul className="slides">
-                        {testimonials}
-                     </ul>
-                  </div>
-               </div>
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">{testimonials}</div>
             </div>
-         </section>
-      );
-   }
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Testimonials;
